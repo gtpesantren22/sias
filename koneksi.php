823578f3f31e5918b33e9bdd944240d6e43a5ca9
gtpesantren22 <cakromat26@gmail.com>
2022-08-29 17:14:52 +0700

    Update koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,9 @@
 <?php
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "";
-	$dbname = "sias";
-	//koneksi
-	$connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-	//cek
-		if(mysqli_connect_errno()){
-			echo 'Koneksi gagal :' .mysqli_connect_errno();
-		}
-?>
+
+//koneksi
+$connect = mysqli_connect("localhost", "root", "", "ams");
+// $conn = mysqli_connect("localhost", "u1492097_smkdwk", "smkdwk2012", "u1492097_rekap");
+//cek
+if (mysqli_connect_errno()) {
+	echo 'Koneksi gagal :' . mysqli_connect_errno();
+}
